45e13d8492374d6e3f342b5fe72524166427ebf9
NikoPan <panayotov.nikolay@gmail.com>
2015-03-31 12:42:53 +0300

    screen width fixed, platforms added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(650, 600, Phaser.CANVAS, 'game');
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game');
 var counter = 0;
 var lives = 3;
 var scoreText;
@@ -11,7 +11,9 @@ var JumpGame = function () {
 
     this.player = null;
     this.platforms = null;
-    this.sky = null;
+    this.background = null;
+    var fruit = null;
+
 
     this.facing = 'left';
     this.edgeTimer = 0;
@@ -28,7 +30,7 @@ JumpGame.prototype = {
 
         this.game.renderer.renderSession.roundPixels = true;
 
-        this.world.resize(650, 2000);
+        this.world.resize(800, 2400);
 
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -39,6 +41,7 @@ JumpGame.prototype = {
 
     preload: function () {
 
+        this.load.image('orange', 'assets/orange.png');
         this.load.image('trees', 'assets/trees.png');
         this.load.image('background', 'assets/background-forest.png');
         this.load.image('platform', 'assets/platform.png');
@@ -49,82 +52,106 @@ JumpGame.prototype = {
 
     create: function () {
 
+
+
+        //this.platforms.setAll('body.allowGravity', false);
+        //this.platforms.setAll('body.immovable', true);
+        //this.platforms.setAll('body.velocity.x', 100);
+
         this.stage.backgroundColor = '#2f9acc';
 
-        this.sky = this.add.tileSprite(0, 0, 800, 600, 'background');
-        this.sky.fixedToCamera = true;
+        this.background = this.add.tileSprite(0, 0, 800, 600, 'background');
+        this.background.fixedToCamera = true;
 
-        this.add.sprite(0, 1700, 'trees');
+        this.add.sprite(0, 2100, 'trees');
 
         this.platforms = this.add.physicsGroup();
+        this.fruit = this.add.physicsGroup();
 
         var x = 0;
         var y = 64;
 
-        for (var i = 0; i < 19; i++)
-        {
-            var type = i % 2 === 1 ? 'platform' : 'dark-platform';
-            var platform = this.platforms.create(x, y, type);
+        for (var i = 0; i < 19; i++) {
+            var platform = this.platforms.create(x, y, 'dark-platform');
 
             //  Set a random speed between 50 and 200
-            platform.body.velocity.x = this.rnd.between(50, 200);
+            platform.body.velocity.x = this.rnd.between(50, 100);
 
             //  Inverse it?
-            if (Math.random() < 0.5)
-            {
+            if (Math.random() < 0.5) {
                 platform.body.velocity.x *= -1;
             }
 
-            x += 300;
+            x += 400;
 
-            if (x >= 600)
-            {
+            if (x >= 800) {
                 x = 0;
             }
 
-            y+= 150;
+            y += 150;
         }
-        
+
+        this.fruit.create(650, 2050, 'orange');
+
+        this.platforms.create(0, 200, 'platform');
+        this.platforms.create(650, 600, 'platform');
+        this.platforms.create(0, 1000, 'platform');
+        this.platforms.create(650, 1300, 'platform');
+        this.platforms.create(0, 1600, 'platform');
+        this.platforms.create(650, 1800, 'platform');
+        this.platforms.create(0, 2000, 'platform');
+        this.platforms.create(650, 2300, 'platform');
+
+
         this.platforms.setAll('body.allowGravity', false);
         this.platforms.setAll('body.immovable', true);
 
-        this.player = this.add.sprite(300, 1900, 'player');
+        this.fruit.setAll('body.allowGravity', false);
+        this.fruit.setAll('body.immovable', true);
+
+
+        this.player = this.add.sprite(game.world.centerX - 100, 2300, 'player');
 
         this.physics.arcade.enable(this.player);
 
         this.player.body.collideWorldBounds = true;
         this.player.body.setSize(100, 100, 0, 0);
 
-        this.player.animations.add('right', [6, 7, 8 , 9 , 10, 11], 7, true);
+        this.player.animations.add('right', [6, 7, 8, 9, 10, 11], 7, true);
         //this.player.animations.add('turn', [0], 20, false);
-        this.player.animations.add('left', [12, 13, 14, 15,16,17], 7, true);
-        this.player.animations.add('down',[20,20], 2, true);
-        this.player.animations.add('dead',[21,21], 2, true);
+        this.player.animations.add('left', [12, 13, 14, 15, 16, 17], 7, true);
+        this.player.animations.add('down', [20, 20], 2, true);
+        this.player.animations.add('dead', [21, 21], 2, true);
 
 
         this.camera.follow(this.player);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        scoreText = game.add.text(10, 1950, 'score: 0', { font: "20px Arial", fill: "yellow", align: "left" });
-        livesText = game.add.text(580, 1950, 'lives: 3', { font: "20px Arial", fill: "yellow", align: "left" });
-        introText = game.add.text(game.world.centerX, 1650, '- click to start -', { font: "40px Arial", fill: "yellow", align: "center" });
+
+        // TODO score and lives need to be fixed at window
+
+        scoreText = game.add.text(10, 2350, 'score: 0', {font: "20px Arial", fill: "yellow", align: "left"});
+        livesText = game.add.text(720, 2350, 'lives: 3', {font: "20px Arial", fill: "yellow", align: "left"});
+        introText = game.add.text(game.world.centerX, 2300, '- click to start -', {
+            font: "40px Arial",
+            fill: "yellow",
+            align: "center"
+        });
         introText.visible = false;
         introText.anchor.setTo(0.5, 0.5);
-        button = game.add.button(game.world.centerX - 95, 400, 'button', this, 2, 1, 0);
+        button = game.add.button(game.world.centerX - 95, 2300, 'button', this, 2, 1, 0);
         button.visible = false;
 
     },
 
     wrapPlatform: function (platform) {
 
-        if (platform.body.velocity.x < 0 && platform.x <= -160)
-        {
-            platform.x = 640;
+        if (platform.body.velocity.x < 0 && platform.x <= -160) {
+            platform.x = 800;
         }
-        else if (platform.body.velocity.x > 0 && platform.x >= 640)
-        {
-            platform.x = -160;
+        else if (platform.body.velocity.x > 0 && platform.x >= 800) {
+            platform.x = -150;
         }
 
     },
@@ -140,51 +167,53 @@ JumpGame.prototype = {
     //
     //},
 
+    fruitEaten: function (player, fruit) {
+        // TODO fruit disappear, score++
+    },
+
     update: function () {
 
-        this.sky.tilePosition.y = -(this.camera.y * 0.7);
+        this.background.tilePosition.y = -(this.camera.y * 0.7);
 
         this.platforms.forEach(this.wrapPlatform, this);
 
-        this.physics.arcade.collide(this.player, this.platforms, this.setFriction, null, this);
+        this.physics.arcade.collide(this.player, this.platforms);
+
+        //this.physics.arcade.collide(this.player, this.platform, this.setFriction, null, this);
+
+
+        this.physics.arcade.overlap(this.player, this.fruit, this.fruitEaten);
+
 
         //  Do this AFTER the collide check, or we won't have blocked/touching set
         var standing = this.player.body.blocked.down || this.player.body.touching.down;
 
         this.player.body.velocity.x = 0;
 
-        if (this.cursors.left.isDown)
-        {
+        if (this.cursors.left.isDown) {
             this.player.body.velocity.x = -200;
 
-            if (this.facing !== 'left')
-            {
+            if (this.facing !== 'left') {
                 this.player.play('left');
                 this.facing = 'left';
             }
         }
-        else if (this.cursors.right.isDown)
-        {
+        else if (this.cursors.right.isDown) {
             this.player.body.velocity.x = 200;
 
-            if (this.facing !== 'right')
-            {
+            if (this.facing !== 'right') {
                 this.player.play('right');
                 this.facing = 'right';
             }
         }
-        else
-        {
-            if (this.facing !== 'idle')
-            {
+        else {
+            if (this.facing !== 'idle') {
                 this.player.animations.stop();
 
-                if (this.facing === 'left')
-                {
+                if (this.facing === 'left') {
                     this.player.frame = 0;
                 }
-                else
-                {
+                else {
                     this.player.frame = 1;
                 }
 
@@ -193,14 +222,12 @@ JumpGame.prototype = {
         }
 
         //  Period to jump after falling
-        if (!standing && this.wasStanding)
-        {
+        if (!standing && this.wasStanding) {
             this.edgeTimer = this.time.time + 250;
         }
 
         //  Allowed to jump?
-        if ((standing || this.time.time <= this.edgeTimer) && this.cursors.up.isDown && this.time.time > this.jumpTimer)
-        {
+        if ((standing || this.time.time <= this.edgeTimer) && this.cursors.up.isDown && this.time.time > this.jumpTimer) {
             //Jumping distance
             this.player.body.velocity.y = -450;
             this.jumpTimer = this.time.time + 750;
@@ -209,7 +236,7 @@ JumpGame.prototype = {
         this.wasStanding = standing;
 
         if (this.player.body.velocity.y >= 700) {
-               counter++;
+            counter++;
         }
 
 
@@ -221,8 +248,7 @@ JumpGame.prototype = {
 
             livesText.text = 'lives: ' + lives;
 
-            if (lives === 0)
-            {
+            if (lives === 0) {
                 console.log('game over');
                 introText.text = 'Game Over!';
                 introText.visible = true;
